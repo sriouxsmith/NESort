00faf02073d1b1df0fc0efad2af3d8564868f2e6
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-05 21:30:55 -0400

    nes_get_buttons -> nes_get_new_buttons - Now returns only newly pressed buttons instead of held

--- src/nes.h
+++ src/nes.h
@@ -9,6 +9,6 @@ void nes_set_ppu_ctrl(uint8_t);
 void nes_set_ppu_mask(uint8_t);
 void nes_clear_nametable(void);
 void nes_set_vram_update(uint8_t len, const uint8_t *src, uint16_t dest);
-uint8_t nes_get_buttons(void);
+uint8_t nes_get_new_buttons(void);
 
 #endif
