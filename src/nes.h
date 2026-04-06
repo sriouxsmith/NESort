90081d8b5a9e7d893fc5fb055507e6a9d9d38115
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-05 22:19:29 -0400

    Added NES button constants for ease of use

--- src/nes.h
+++ src/nes.h
@@ -4,6 +4,15 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#define NES_BUTTON_A      0x80
+#define NES_BUTTON_B      0x40
+#define NES_BUTTON_SELECT 0x20
+#define NES_BUTTON_START  0x10
+#define NES_BUTTON_UP     0x08
+#define NES_BUTTON_DOWN   0x04
+#define NES_BUTTON_LEFT   0x02
+#define NES_BUTTON_RIGHT  0x01
+
 extern uint8_t nes_vram_buffer[32];
 
 void nes_wait_frame(void);
