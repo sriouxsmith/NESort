fb76b1f4e522207b0d0edfc26d53970dba927b6d
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-06 11:09:22 -0400

    Added array module - Implemented functions for retrieving and changing settings (required for implementation of the option screen)

---
+++ src/array.h
@@ -0,0 +1,11 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include "nes.h"
+
+uint8_t arr_get_update_speed(void);
+void arr_set_update_speed(uint8_t);
+bool arr_is_aux_visible(void);
+void arr_set_aux_visible(bool);
+
+#endif
