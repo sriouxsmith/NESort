e1732d4beac97cdefacaba04165ba7776a642c61
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-05 21:02:07 -0400

    nes_clear_nametables -> nes_clear_nametable - Simplify logic by moving everything to the left nametable

--- src/nes.h
+++ src/nes.h
@@ -7,7 +7,7 @@
 void nes_wait_frame(void);
 void nes_set_ppu_ctrl(uint8_t);
 void nes_set_ppu_mask(uint8_t);
-void nes_clear_nametables(void);
+void nes_clear_nametable(void);
 void nes_set_vram_update(uint8_t len, const uint8_t *src, uint16_t dest);
 uint8_t nes_get_buttons(void);
 
