572edfb0042bef379d5180b9f6e64c58d0475e6c
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-05 20:18:28 -0400

    Implemented nes_get_buttons

--- src/nes.h
+++ src/nes.h
@@ -9,5 +9,6 @@ void nes_set_ppu_ctrl(uint8_t);
 void nes_set_ppu_mask(uint8_t);
 void nes_clear_nametables(void);
 void nes_set_vram_update(uint8_t len, const uint8_t *src, uint16_t dest);
+uint8_t nes_get_buttons(void);
 
 #endif
