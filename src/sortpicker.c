724cfb967d96e0922b277a368a8d2272cf98a6c7
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-05 21:55:30 -0400

    Added sample Heap sort description to test text renderer

--- src/sortpicker.c
+++ src/sortpicker.c
@@ -1,5 +1,31 @@
 #include "sortpicker.h"
 
+static const char *const sort_text[] = {
+	"\xa4\xa5 Heap Sort                 3\n"
+	"\xb4\xb5\n"
+	"\n"
+	"An improvement on selection\n"
+	"sort which first organizes the\n"
+	"data into a binary max heap\n"
+	"and uses it to find the next\n"
+	"highest item in logarithmic\n"
+	"time. This is done by swapping\n"
+	"the top parent with the last\n"
+	"child, removing it from the\n"
+	"tree, and then reordering to\n"
+	"satisfy the heap property.n\n"
+	"\n"
+	"The heap is stored implicitly\n"
+	"on the original array. For any\n"
+	"parent (index i), the left\n"
+	"child is at 2i + 1 and the\n"
+	"right child at 2i + 2.\n"
+	"\n"
+	"Time:   O(nlogn)\n"
+	"Space:  O(1)\n"
+	"Stable: no"
+};
+
 static void init(void) {
 	nes_set_ppu_ctrl(0x18);
 	nes_set_ppu_mask(0);
