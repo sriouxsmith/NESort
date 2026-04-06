07087cfba5e37bb2d2c00ad3e836c9ec77963d34
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-06 13:24:46 -0400

    Added random numbers and seed (nes_seed_rand, nes_get_rand8)

--- src/nes.h
+++ src/nes.h
@@ -23,5 +23,7 @@ void nes_set_ppu_mask(uint8_t);
 void nes_clear_nametable(void);
 void nes_set_vram_update(uint8_t len, const uint8_t *src, uint16_t dest);
 uint8_t nes_get_new_buttons(void);
+void nes_seed_rand(uint32_t);
+uint8_t nes_get_rand8(void);
 
 #endif
