0ae0038d8341f57870f121270b03367e6bc7ac48
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-06 11:24:25 -0400

    Added Pipeline structure to contain the three demo functions and the length/number of distincts - No implementation of the pipeline yet

---
+++ src/pipeline.h
@@ -0,0 +1,15 @@
+#ifndef PIPELINE_H
+#define PIPELINE_H
+
+#include "nes.h"
+
+typedef struct {
+	void (*distribute)(uint8_t len, uint8_t distincts);
+	void (*shuffle)(uint8_t len);
+	void (*sort)(uint8_t len);
+
+	uint8_t len;
+	uint8_t distincts;
+} Pipeline;
+
+#endif
