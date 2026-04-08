52d522aba1ec02471cb953888f4083a039128486
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-07 22:40:44 -0400

    Added declarations for all four arrays in array.h/a65

--- src/array.h
+++ src/array.h
@@ -3,6 +3,16 @@
 
 #include "nes.h"
 
+#define MAX_LEN 120
+
+/* Displayable */
+extern uint8_t arr_main[MAX_LEN];
+extern uint8_t arr_aux[MAX_LEN];
+
+/* Invisible */
+extern uint8_t arr_ext0[MAX_LEN];
+extern uint8_t arr_ext1[MAX_LEN];
+
 uint8_t arr_get_update_speed(void);
 void arr_set_update_speed(uint8_t);
 bool arr_is_aux_visible(void);
