7a3aa9da2fb3f8564723c6e87fd53d3db4dbd148
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-07 21:18:52 -0400

    Implemented bcd_render_as_spr - Used for options screen to render BCD digits - May be used during the sort to show operation count

--- src/bcd.c
+++ src/bcd.c
@@ -28,3 +28,27 @@ void bcd_from_binary(uint8_t value, uint8_t *bcd_out) {
 	bcd[1] &= 0x0f;
 	bcd[2] &= 0x0f;
 }
+
+uint8_t bcd_render_as_spr(uint8_t x, uint8_t y, uint8_t index,
+		uint8_t len, uint8_t *bcd)
+{
+	static uint8_t *bcd_working;
+
+	static uint8_t new_x;
+	static uint8_t new_index;
+	static uint8_t new_len;
+	static uint8_t i;
+
+	bcd_working = bcd;
+	for (new_len = len; new_len > 1 && !bcd_working[new_len - 1]; --new_len);
+
+	new_x = 8 * (new_len - 1) + x;
+	new_index = index;
+
+	for (i = 0; i < new_len; ++i) {
+		new_index = nes_put_spr(new_x, y, bcd[i] + 0x30, 0, new_index);
+		new_x -= 8;
+	}
+
+	return new_index;
+}
--- src/bcd.h
+++ src/bcd.h
@@ -4,5 +4,9 @@
 #include "nes.h"
 
 void bcd_from_binary(uint8_t value, uint8_t *bcd_out);
+// returns the next available index
+uint8_t bcd_render_as_spr(uint8_t x, uint8_t y, uint8_t index,
+		uint8_t len, uint8_t *bcd);
+
 
 #endif
