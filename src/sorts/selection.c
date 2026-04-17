51b1502809243d21b524ab190e20a24aa54a3ff9
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-16 23:12:01 -0400

    Added stub for selection sort

---
+++ src/sorts/selection.c
@@ -0,0 +1,7 @@
+#include "selection.h"
+
+#include "../array.h"
+
+void selection_sort(uint8_t len) {
+
+}
---
+++ src/sorts/selection.h
@@ -0,0 +1,8 @@
+#ifndef SELECTION_H
+#define SELECTION_H
+
+#include "../nes.h"
+
+void selection_sort(uint8_t len);
+
+#endif
