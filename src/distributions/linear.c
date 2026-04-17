d05d22c768da9a164aad852f341e61b4de175981
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-16 23:08:09 -0400

    Added stub for linear_distribution

---
+++ src/distributions/linear.c
@@ -0,0 +1,7 @@
+#include "linear.h"
+
+#include "../array.h"
+
+void linear_distribution(uint8_t len, uint8_t distincts) {
+
+}
---
+++ src/distributions/linear.h
@@ -0,0 +1,8 @@
+#ifndef LINEAR_H
+#define LINEAR_H
+
+#include "../nes.h"
+
+void linear_distribution(uint8_t len, uint8_t distincts);
+
+#endif
