a9cbb086387988cd6690d7b348f2a193ec52ffa7
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-16 23:10:18 -0400

    Added stub for Fisher-Yates shuffling

---
+++ src/shuffles/fisheryates.c
@@ -0,0 +1,7 @@
+#include "fisheryates.h"
+
+#include "../array.h"
+
+void fisheryates_shuffle(uint8_t len) {
+
+}
---
+++ src/shuffles/fisheryates.h
@@ -0,0 +1,8 @@
+#ifndef FISHERYATES_H
+#define FISHERYATES_H
+
+#include "../nes.h"
+
+void fisheryates_shuffle(uint8_t len);
+
+#endif
