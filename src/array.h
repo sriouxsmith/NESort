6c19fb644c1af4b0b6ab33217c9e6882a9d44d83
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-16 22:35:01 -0400

    Implemented arr_swap - MILESTONE: array interface now (functionally) complete

--- src/array.h
+++ src/array.h
@@ -24,9 +24,11 @@ void arr_push_update(uint8_t col);
 void arr_push_blank(void);
 
 uint8_t arr_read(bool aux, uint8_t i);
-void arr_write(bool aux, uint8_t i, uint8_t val);
 int8_t arr_compare(uint8_t left, uint8_t right);
 int8_t arr_compare_indices(bool aux, uint8_t left, uint8_t right);
+
+void arr_write(bool aux, uint8_t i, uint8_t val);
+void arr_swap(bool aux, uint8_t a, uint8_t b);
 
 void arr_play(uint8_t val);
 
