da153b0c03f94b540ee6417a52bb1968f654aba4
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-18 15:53:32 -0400

    Implemented selection sort

--- src/sorts/selection.c
+++ src/sorts/selection.c
@@ -3,5 +3,16 @@
 #include "../array.h"
 
 void selection_sort(uint8_t len) {
+	uint8_t i, j;
 
+	for (i = 0; i < len - 1; ++i) {
+		uint8_t min = i;
+
+		for (j = i + 1; j < len; ++j) {
+			if (arr_compare_indices(false, j, min) < 0)
+				min = j;
+		}
+
+		arr_swap(false, i, min);
+	}
 }
