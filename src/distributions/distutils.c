2cb6272faf449f6d7611fa9625b41495098491a1
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-18 00:20:15 -0400

    Implemented distribute_standard (will be used for most/all distribution functions

---
+++ src/distributions/distutils.c
@@ -0,0 +1,15 @@
+#include "distutils.h"
+
+#include "../array.h"
+
+void distribute_standard(uint8_t len, uint8_t distincts, const uint8_t *base) {
+	uint8_t i;
+	for (i = 0; i < len; ++i) {
+		if (distincts == 1)
+			arr_write(false, i, 60);
+		else arr_write(false, i,
+				i * distincts
+				/ len * (ARR_MAX_LEN - 1)
+				/ (distincts - 1) + 1);
+	}
+}
---
+++ src/distributions/distutils.h
@@ -0,0 +1,8 @@
+#ifndef DISTUTILS_H
+#define DISTUTILS_H
+
+#include "../nes.h"
+
+void distribute_standard(uint8_t len, uint8_t distincts, const uint8_t *base);
+
+#endif
