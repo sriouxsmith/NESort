72f52078fc04d80dc0abdb1ec3844ff8625600dc
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-18 13:56:26 -0400

    Implemented Fisher-Yates shuffling

--- src/shuffles/fisheryates.c
+++ src/shuffles/fisheryates.c
@@ -3,5 +3,10 @@
 #include "../array.h"
 
 void fisheryates_shuffle(uint8_t len) {
+	uint8_t i, j;
 
+	for (i = 0; i < len; ++i) {
+		j = nes_get_rand_bounded(i + 1);
+		arr_swap(false, i, j);
+	}
 }
