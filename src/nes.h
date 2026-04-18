2bc1dd78529b6dff336ec4f8d450cb23066896f8
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-18 13:55:26 -0400

    Implemented nes_get_rand_bounded, using rejection sampling to cap random values

--- src/nes.h
+++ src/nes.h
@@ -25,6 +25,7 @@ void nes_set_vram_update(uint8_t len, const uint8_t *src, uint16_t dest);
 uint8_t nes_get_new_buttons(void);
 void nes_seed_rand(uint32_t);
 uint8_t nes_get_rand8(void);
+uint8_t nes_get_rand_bounded(uint8_t upper);
 void nes_hide_spr(uint8_t start_index);
 // returns the next available sprite index
 uint8_t nes_put_spr(uint8_t x, uint8_t y, uint8_t tile, uint8_t attr, uint8_t index);
