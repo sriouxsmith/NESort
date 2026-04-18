6c0bff74eccd9a93de5201b21f778baef845fac2
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-18 17:58:55 -0400

    FIX: >= 0 instead of >= 1 for gnome_sort (would fail on non-uniques)

--- src/sorts/gnome.c
+++ src/sorts/gnome.c
@@ -6,7 +6,7 @@ void gnome_sort(uint8_t len) {
 	uint8_t i = 1;
 
 	while (i < len) {
-		if (!i || arr_compare_indices(false, i, i - 1) >= 1)
+		if (!i || arr_compare_indices(false, i, i - 1) >= 0)
 			++i;
 		else {
 			arr_swap(false, i, i - 1);
