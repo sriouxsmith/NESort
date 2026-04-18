9e418321588c33a907053c97c9ed902dc27c3c9d
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-18 00:22:04 -0400

    Implemented linear_distribution - TODO: optimize (only runs at 30 FPS)

--- src/distributions/linear.c
+++ src/distributions/linear.c
@@ -1,7 +1,12 @@
 #include "linear.h"
+#include "distutils.h"
 
 #include "../array.h"
 
 void linear_distribution(uint8_t len, uint8_t distincts) {
+	uint8_t i;
+	for (i = 0; i < ARR_MAX_LEN; ++i)
+		arr_ext0[i] = i + 1;
 
+	distribute_standard(len, distincts, arr_ext0);
 }
