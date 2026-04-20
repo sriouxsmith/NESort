384c110ed8b8a6dbca80cdb29a69ce4e67708333
Shannon Rioux-Smith <sriouxsmith@gmail.com>
2026-04-19 22:49:21 -0400

    Made insertion sort swapless

--- src/sorts/insertion.c
+++ src/sorts/insertion.c
@@ -4,12 +4,16 @@
 
 void insertion_sort(uint8_t len) {
 	uint8_t i, j;
+	uint8_t e;
 
 	for (j = 1; j < len; ++j) {
+		e = arr_read(false, j);
+
 		for (i = j - 1; i < len; --i) {
-			if (arr_compare_indices(false, i, i + 1) <= 0)
+			if (arr_compare(arr_read(false, i), e) <= 0)
 				break;
-			arr_swap(false, i, i + 1);
+			arr_write(false, i + 1, arr_read(false, i));
 		}
+		arr_write(false, i + 1, e);
 	}
 }
